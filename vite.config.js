004adf689837c3a0bd973a6589df86d7c975cfca
pvictor1206 <paulomagalhaes1206@gmail.com>
2024-09-05 14:48:32 -0300

    commit

---
+++ vite.config.js
@@ -0,0 +1,6 @@
+export default {
+    root: 'public', // Define 'public' como o diretório raiz
+    build: {
+      outDir: '../dist', // Define onde os arquivos compilados serão colocados
+    }
+  };
