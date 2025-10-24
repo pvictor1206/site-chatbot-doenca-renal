39a7e59e119901562026119db7a85e94c6d524fe
Paulo Victor Santos Magalhães <paulomagalhaes1206@gmail.com>
2025-10-24 11:00:01 -0300

    FINAL - VERSAO 1.0

--- public/js/firebase-init.js
+++ public/js/firebase-init.js
@@ -7,13 +7,7 @@ import { getFirestore } from "https://www.gstatic.com/firebasejs/9.23.0/firebase
 
 // Configurações do Firebase
 const firebaseConfig = {
-  apiKey: "AIzaSyACe0H1mia7FrM3_3C131adLwV1BVVGCvQ",
-  authDomain: "chat-doenca-renal.firebaseapp.com",
-  projectId: "chat-doenca-renal",
-  storageBucket: "chat-doenca-renal.appspot.com",
-  messagingSenderId: "378280331607",
-  appId: "1:378280331607:web:1ed58036b69356b1655612",
-  measurementId: "G-2VFMSCE9BP"
+ 
 };
 
 // Inicializa o Firebase
