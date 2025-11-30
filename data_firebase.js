0025a3341d11ab446ef4fb9509572448ef384968
Paulo Victor Santos Magalhães <paulomagalhaes1206@gmail.com>
2025-11-30 16:13:44 -0300

    commit

--- data_firebase.js
+++ data_firebase.js
@@ -7,7 +7,13 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-
+  apiKey: "AIzaSyACe0H1mia7FrM3_3C131adLwV1BVVGCvQ",
+  authDomain: "chat-doenca-renal.firebaseapp.com",
+  projectId: "chat-doenca-renal",
+  storageBucket: "chat-doenca-renal.appspot.com",
+  messagingSenderId: "378280331607",
+  appId: "1:378280331607:web:1ed58036b69356b1655612",
+  measurementId: "G-2VFMSCE9BP"
 };
 
 // Initialize Firebase
